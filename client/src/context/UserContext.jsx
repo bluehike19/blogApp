57e4b94cbfa9b58aabe82f3a7d6ac0fa391c0991
simo blues <wambuisimon712@gmail.com>
2024-02-13 06:54:53 +0300

    changes

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -1,2 +1,24 @@
 import axios from 'axios'
 import { URL } from '../url'
+import { createContext, useEffect, useState } from 'react'
+
+export const UserContext = createContext({})
+
+export function UserContextProvider({children}) {
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        getUser()
+    },[])
+
+    const getUser = async() => {
+        try {
+           const res = await axios.get(URL + '/api/auth/refetch', {withCredentials:true})
+           setUser(res.data) 
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    return (<UserContext.Provider value={{user, setUser}}>{children}</UserContext.Provider>)
+}
