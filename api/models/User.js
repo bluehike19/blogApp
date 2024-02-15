17037ea29aee963c5686f9ca71ef92e63dcd3294
simo blues <wambuisimon712@gmail.com>
2024-02-15 06:47:27 +0300

    changes

--- api/models/User.js
+++ api/models/User.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose')
 
-const UserSchema = new mongoose.Schema({})
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+})
 
 module.exports = mongoose.models("User", UserSchema)
