20a8656858a0600ad8c8948d798fcc7a80cc81de
simo blues <wambuisimon712@gmail.com>
2024-02-15 06:54:23 +0300

    changes

--- api/models/Post.js
+++ api/models/Post.js
@@ -1,19 +1,30 @@
 const mongoose = require('mongoose')
 
 const PostSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    desc: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    photo: {
+        type: String,
+        required: false,
+    },
     username: {
         type: String,
         required: true,
-        unique: true
     },
-    email: {
+    userId: {
         type: String,
         required: true,
-        unique: true
     },
-    password: {
-        type: String,
-        required: true,
+    categories: {
+        type: Array,
     },
 }, {timestamps: true})
 
