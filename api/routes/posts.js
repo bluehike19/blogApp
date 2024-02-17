a84ab6b8bc78321a88a2c8d261479b46f068f964
simo blues <wambuisimon712@gmail.com>
2024-02-17 06:54:11 +0300

    changes

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,4 +1,18 @@
 const express = require('express')
+const verifyToken = require('../verifyToken')
+const Post = require('../models/Post')
 const router = express.Router()
+
+//CREATE
+router.post("/create", verifyToken, async (req, res) => {
+    try {
+        const newPost = new Post(req.body)
+        const savedPost = await newPost.save()
+
+        res.status(200).json(savedPost)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router
