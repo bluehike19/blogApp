077a028602fcca9f04968ab543ce4059fa3748e2
simo blues <wambuisimon712@gmail.com>
2024-02-17 07:00:46 +0300

    changes

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -15,4 +15,25 @@ router.post("/create", verifyToken, async (req, res) => {
     }
 })
 
+//UPDATE
+router.put("/:id", verifyToken, async (req, res) => {
+    try {
+        const updatedPost = await Post.findByIdAndUpdate(req.params.id, {$set:req.body},{new:true})
+        res.status(200).json(updatedPost)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+//DELETE
+router.delete("/:id", verifyToken, async (req, res) => {
+    try {
+        await Post.findByIdAndDelete(req.params.id)
+        await Post.deleteMany({postId:req.params.id})
+        res.status(200).json("Post has been deleted!")
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
 module.exports = router
