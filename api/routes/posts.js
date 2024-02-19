6cba9eed40845287b475bae38d32050f9b94586e
simo blues <wambuisimon712@gmail.com>
2024-02-19 05:38:42 +0300

    changes

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const verifyToken = require('../verifyToken')
 const Post = require('../models/Post')
+const Comment = require('../models/Comment')
 const router = express.Router()
 
 //CREATE
@@ -29,7 +30,7 @@ router.put("/:id", verifyToken, async (req, res) => {
 router.delete("/:id", verifyToken, async (req, res) => {
     try {
         await Post.findByIdAndDelete(req.params.id)
-        await Post.deleteMany({postId:req.params.id})
+        await Comment.deleteMany({postId:req.params.id})
         res.status(200).json("Post has been deleted!")
     } catch (err) {
         res.status(500).json(err)
