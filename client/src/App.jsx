f4f8c36c5eeb5fba2c4be7ef55bc9cfb4f161a13
simo blues <wambuisimon712@gmail.com>
2024-02-20 06:07:00 +0300

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,17 @@ import {Route, Routes} from 'react-router-dom'
 import './App.css'
 import { UserContextProvider } from './context/UserContext'
 import Login from './pages/Login'
+import Home from './pages/Home'
+import Register from './pages/Register'
 
 function App() {
 
   return (
  <UserContextProvider>
   <Routes>
+    <Route exact path='/' element={<Home />} />
     <Route exact path='/login' element={<Login /> } />
+    <Route exact path='/register' element={<Register /> } />
   </Routes>
  </UserContextProvider>
   )
