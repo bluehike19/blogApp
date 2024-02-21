18daf05d54980bae964f938b7be337313151bcb6
simo blues <wambuisimon712@gmail.com>
2024-02-21 15:16:23 +0300

    changes

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -30,7 +30,8 @@ const Login = () => {
     <div className='w-full flex justify-center items-center h-[80vh]'>
       <div className='flex flex-col justify-center items-center space-y-4 w-[80%] md:w-[25%]'>
         <h1 className='text-xl font-bold text-left'>Log in to your account</h1>
-        <input className='w-full px-4 py-2 border-2 border-black outline-0' placeholder='Enter your email' type="text" />
+        <input onChange={(e)=>setEmail(e.target.value)} className='w-full px-4 py-2 border-2 border-black outline-0' placeholder='Enter your email' type="text" />
+        <input onChange={(e)=>setPassword(e.target.value)} className='w-full px-4 py-2 border-2 border-black outline-0' placeholder='Enter your password' type="pasword" />
       </div>
     </div>
     </>
