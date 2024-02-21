05a327649ebb2667d71d91f9f50fd3d1d978ee34
simo blues <wambuisimon712@gmail.com>
2024-02-21 15:04:00 +0300

    changes

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -27,6 +27,11 @@ const Login = () => {
       <h1 className='text-lg md:text-xl font-extrabold'><Link to='/'>Blog Market</Link></h1>
       <h3><Link to='/register'>Register</Link></h3>
     </div>
+    <div className='w-full flex justify-center items-center h-[80vh]'>
+      <div className='flex flex-col justify-center items-center space-y-4 w-[80%] md:w-[25%]'>
+        <h1 className='text-xl font-bold text-left text-red-500'>Log in to your account</h1>
+      </div>
+    </div>
     </>
   )
 }
