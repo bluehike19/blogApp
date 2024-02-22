e0f92c86362b97a2a19e9babb241b8b9bcab668d
simo blues <wambuisimon712@gmail.com>
2024-02-22 05:52:20 +0300

    changes

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
         try {
             const res = await axios.post(URL+"/api/auth/login",{email,password},{withCredentials:true})
             setUser(res.data)
-            NavigationPreloadManager("/")
+            navigate("/")
         } catch (err) {
             setError(true)
             console.log(err)
@@ -34,6 +34,12 @@ const Login = () => {
         <h1 className='text-xl font-bold text-left'>Log in to your account</h1>
         <input onChange={(e)=>setEmail(e.target.value)} className='w-full px-4 py-2 border-2 border-black outline-0' placeholder='Enter your email' type="text" />
         <input onChange={(e)=>setPassword(e.target.value)} className='w-full px-4 py-2 border-2 border-black outline-0' placeholder='Enter your password' type="pasword" />
+        <button onClick={handleLogin} className='w-full px-4 py-4 text-lg font-bold text-white bg-black rounded-lg hover:bg-gray-500 hover:text-black'>Log in</button>
+        {error && <h3 className='text-red-500 text-sm'>Something went wrong</h3>}
+        <div className='flex justify-center items-center space-x-3'>
+          <p>New here?</p>
+          <p className='text-gray-500 hover:text-black'><Link to='/register'>Register</Link></p>
+        </div>
       </div>
     </div>
     </>
