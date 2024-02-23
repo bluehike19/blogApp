8ce11a361ee06e4eac7d6f5400f73e26b7c3ff59
simo blues <wambuisimon712@gmail.com>
2024-02-23 06:20:26 +0300

    changes

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -34,9 +34,10 @@ const Register = () => {
     <div className='w-full flex justify-center items-center'>
       <div className='flex flex-col justify-center items-center space-y-4 w-[80%] md:w-[25%]'>
         <h1>Create an account</h1>
-        <input onChange={(e)=>setEmail(e.target.value)} className='w-full px-4 py-2 border-2 border-black outline-0' type="text" placeholder='Enter ' />
-        <input onChange={(e)=>setEmail(e.target.value)} className='w-full px-4 py-2 border-2 border-black outline-0' type="text" placeholder='Enter ' />
-        <input onChange={(e)=>setEmail(e.target.value)} className='w-full px-4 py-2 border-2 border-black outline-0' type="text" placeholder='Enter ' />
+        <input onChange={(e)=>setUsername(e.target.value)} className='w-full px-4 py-2 border-2 border-black outline-0' type="text" placeholder='Enter your username' />
+        <input onChange={(e)=>setPassword(e.target.value)} className='w-full px-4 py-2 border-2 border-black outline-0' type="text" placeholder='Enter your email' />
+        <input onChange={(e)=>setEmail(e.target.value)} className='w-full px-4 py-2 border-2 border-black outline-0' type="text" placeholder='Enter your password'/>
+        <button onClick={handleRegister} className='w-full px-4 py-4 text-lg font-bold text-white bg-black rounded-lg hover:bg-gray-500 hover:text-black'>Register</button>
       </div>
     </div>
     </>
