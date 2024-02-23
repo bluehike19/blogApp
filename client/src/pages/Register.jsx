a04dc886ea470af01074c37690e62a6fafef6bbe
simo blues <wambuisimon712@gmail.com>
2024-02-23 06:50:11 +0300

    changes

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import React, { useState } from 'react'
 import { URL } from '../url'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Register = () => {
 
