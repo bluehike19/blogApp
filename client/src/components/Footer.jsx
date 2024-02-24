cde898fc07cd6542465e98d753af4363b5a0b696
simo blues <wambuisimon712@gmail.com>
2024-02-24 05:57:23 +0300

    changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+    <div className='mt-8 w-full bg-black px-8 md:px-[300px] flex md:flex-row flex-col space-y-6 md:space-y-0 items-start md:justify-between text-sm md:text-md py-8'>
+        <div className="flex flex-col text-white">
+            <p>Featured Blogs</p>
+            <p>Most Viewed</p>
+            <p>Readers choice</p>
+        </div>
+        
+    </div>
+    </>
   )
 }
 
