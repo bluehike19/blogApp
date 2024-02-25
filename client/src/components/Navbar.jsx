29871f97573843ec87adc593719ee49fa75db03b
simo blues <wambuisimon712@gmail.com>
2024-02-25 06:57:06 +0300

    changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 const Navbar = () => {
   const [prompt, setPrompt] = useState("")
   const [menu, setMenu] = useState(false)
+  const navigate = useNavigate()
   return (
     <div>Navbar</div>
   )
