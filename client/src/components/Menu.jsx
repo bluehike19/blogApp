b627ff1e498bfe4fe1b3c34b7e641a646c8ea416
simo blues <wambuisimon712@gmail.com>
2024-02-27 06:55:59 +0300

    changes

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -21,10 +21,11 @@ const Menu = () => {
   return (
     <div className='bg-black w-[200px] z-10 flex flex-col items-start absolute top-12 right-6 md:right-32 rounded-md p-4 space-y-4'>
       {!user && <h3 className='text-white text-sm hover:text-gray-500 cursor-pointer'><Link to='/login'>Login</Link></h3>}
-      {!user && <h3 className='text-white text-sm hover:text-gray-500 cursor-pointer'><Link to='/register'></Link>Register</h3>}
-      {user && <h3 className='text-white text-sm hover:text-gray-500 cursor-pointer'><Link to={"/profile/"+user._id}>profile</Link>Register</h3>}
-      {!user && <h3 className='text-white text-sm hover:text-gray-500 cursor-pointer'><Link to='/write'></Link>Write</h3>}
-      {user && <h3 className='text-white text-sm hover:text-gray-500 cursor-pointer'><Link to={"/myblogs"+user._id}></Link>My blogs</h3>}
+      {!user && <h3 className='text-white text-sm hover:text-gray-500 cursor-pointer'><Link to='/register'>Register</Link></h3>}
+      {user && <h3 className='text-white text-sm hover:text-gray-500 cursor-pointer'><Link to={"/profile/"+user._id}>profile</Link></h3>}
+      {!user && <h3 className='text-white text-sm hover:text-gray-500 cursor-pointer'><Link to='/write'>Write</Link></h3>}
+      {user && <h3 className='text-white text-sm hover:text-gray-500 cursor-pointer'><Link to={"/myblogs"+user._id}>My blogs</Link>My blogs</h3>}
+      {user &&<h3 onClick={handleLogout} className='text-white text-sm hover:text-gray-500 cursor-pointer'>Logout</h3>}
     </div>
   )
 }
