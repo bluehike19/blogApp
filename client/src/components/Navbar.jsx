fe23420a50c1f243f6ef36b2f8005fda8ec486d2
simo blues <wambuisimon712@gmail.com>
2024-02-27 06:58:16 +0300

    changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
   const [prompt, setPrompt] = useState("")
   const [menu, setMenu] = useState(false)
   const navigate = useNavigate()
+  const path = useLocation().pathname
   return (
     <div>Navbar</div>
   )
