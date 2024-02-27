3587bc901e62472fe5bbfc0ac8dfa3a6a5e4a75b
simo blues <wambuisimon712@gmail.com>
2024-02-27 06:36:43 +0300

    changes

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,9 +1,23 @@
 import React, { useContext } from 'react'
 import { UserContext } from '../context/UserContext'
+import axios from 'axios'
+import { URL } from '../url'
+import { useNavigate } from 'react-router-dom'
 
 const Menu = () => {
   const { user } = useContext(UserContext)
   const { setUser } = useContext(UserContext)
+  const navigate = useNavigate()
+
+  const handleLogout = async()=> {
+    try {
+      const res = await axios.get(URL+"/api/auth/logout",{withCredentials:true})
+      setUser(null)
+      navigate("/login")
+    } catch (err) {
+      console.log(err)
+    }
+  }
   return (
     <div>Menu</div>
   )
