2c6c8ea29ed7e690e09cd65e2deb1becb1315041
simo blues <wambuisimon712@gmail.com>
2024-02-29 06:27:09 +0300

    changes

--- client/src/components/HomePost.jsx
+++ client/src/components/HomePost.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { IF } from '../url'
 
 const HomePost = () => {
   return (
-    <div>HomePost</div>
+    <div className='w-full flex mt-8 space-x-4'>
+      <div className="w-[35%] h-[200px] flex justify-center items-center">
+        <img src={IF+postMessage.photo} alt="" className='h-full w-full object-cover' />
+      </div>
+      <div className="flex flex-col w-[65%]">
+        <h1 className='text-xl font-bold md:mb-2 mb-1 md:text-2xl'>{post.title}</h1>
+      </div>
+    </div> 
   )
 }
 
