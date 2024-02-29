811cce3fc8c1e1c056615a84e033ae152936269f
simo blues <wambuisimon712@gmail.com>
2024-02-29 06:32:53 +0300

    changes

--- client/src/components/HomePost.jsx
+++ client/src/components/HomePost.jsx
@@ -9,6 +9,14 @@ const HomePost = () => {
       </div>
       <div className="flex flex-col w-[65%]">
         <h1 className='text-xl font-bold md:mb-2 mb-1 md:text-2xl'>{post.title}</h1>
+        <div className='flex mb-2 text-sm font-semibold text-gray-500 items-center justify-between md:mb-4'>
+          <p>@{post.username}</p>
+          <div className="flex space-x-2 text-sm">
+            <p>{new Date(post.updatedAt).toString().slice(0,15)}</p>
+            <p>{new Date(post.updatedAt).toString().slice(16,24)}</p>
+          </div>
+        </div>
+        <p className="text-sm md:text-lg">{post.desc.slice(0,200)+" ...Read more"}</p>
       </div>
     </div> 
   )
