daf1586d16943210f9a3b5da088323a7de8e9c44
simo blues <wambuisimon712@gmail.com>
2024-02-29 05:48:42 +0300

    changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React, { useContext, useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
-import { useLocation } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { UserContext } from '../context/UserContext'
 import axios from 'axios'
+import Loader from '../components/Loader'
 
 const Home = () => {
 
@@ -37,6 +38,13 @@ const Home = () => {
   return (
     <>
     <Navbar />
+    <div className="px-8 md:px-[200px] min-h-[80vh]">
+      {loader?<div className='h-[40vh] flex justify-center items-center'><Loader /></div> :!noResults?posts.map((post)=>(
+      <>
+      <Link to={user?`/posts/post/${post._id}`:"/login"}></Link>
+      </>
+      )): <h3 className='text-center font-bold mt-16'>No posts available</h3>}
+    </div>
     </>
   )
 }
