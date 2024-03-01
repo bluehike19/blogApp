fea2308eb96113acdf41ecdf1c23d2dfc642f875
simo blues <wambuisimon712@gmail.com>
2024-03-01 05:06:16 +0300

    photos

--- client/src/components/ProfilePost.jsx
+++ client/src/components/ProfilePost.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { IF } from '../url'
 
 const ProfilePost = () => {
   return (
-    <div>ProfilePost</div>
+    <div className='w-full flex mt-8 space-x-4'>
+      <div className="w-[35%] h-[200px] flex justify-center items-center">
+        <img src={IF+p.photo} alt="" className='h-full w-full object-cover' />
+      </div>
+      <div className="flex flex-col w-[65%]">
+        <h1></h1>
+      </div>
+    </div>
   )
 }
 
