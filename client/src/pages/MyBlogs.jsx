e4727621fa1683ac0cc2633a6397361dedc4e7e7
simo blues <wambuisimon712@gmail.com>
2024-03-02 22:37:47 +0300

    changes

--- client/src/pages/MyBlogs.jsx
+++ client/src/pages/MyBlogs.jsx
@@ -4,6 +4,7 @@ const MyBlogs = () => {
     const {search} = useLocation()
     const [posts, setPosts] = useState([])
     const [noResults, setNoResults] = useState(false)
+    const [loader, setLoader] = useState(false)
   return (
     <div>MyBlogs</div>
   )
