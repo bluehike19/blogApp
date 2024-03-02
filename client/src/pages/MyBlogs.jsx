9582f0610c98ff1953fd347c0aa82ffe84e8b3bb
simo blues <wambuisimon712@gmail.com>
2024-03-02 22:36:38 +0300

    changes

--- client/src/pages/MyBlogs.jsx
+++ client/src/pages/MyBlogs.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 const MyBlogs = () => {
     const {search} = useLocation()
     const [posts, setPosts] = useState([])
+    const [noResults, setNoResults] = useState(false)
   return (
     <div>MyBlogs</div>
   )
