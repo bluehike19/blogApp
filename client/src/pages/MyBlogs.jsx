0dfef4f873f17049b3d4069e462fbd9e37b7d01d
simo blues <wambuisimon712@gmail.com>
2024-03-02 22:38:55 +0300

    changes

--- client/src/pages/MyBlogs.jsx
+++ client/src/pages/MyBlogs.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 
 const MyBlogs = () => {
     const {search} = useLocation()
     const [posts, setPosts] = useState([])
     const [noResults, setNoResults] = useState(false)
     const [loader, setLoader] = useState(false)
+    const { user } = useContext(userContext) 
   return (
     <div>MyBlogs</div>
   )
