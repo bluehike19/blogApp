ee3e003f344cf6dd41fea93d654a71f80af89913
simo blues <wambuisimon712@gmail.com>
2024-03-03 06:51:25 +0300

    changes

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { UserContext } from '../context/UserContext'
+import { useNavigate } from 'react-router-dom'
 
 const CreatePost = () => {
+  const [title, setTitle] = useState('')
+  const [desc, setDesc] = useState('')
+  const [file, setFile] = useState('')
+  const { user } = useContext(UserContext)
+  const [cat, setCat] = useState('')
+  const [cats, setCats] = useState([])
+
+  const navigate = useNavigate()
   return (
     <div>CreatePost</div>
   )
