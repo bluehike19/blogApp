0f2d0ff35e827be27e81807c1de2e4074a975a04
simo blues <wambuisimon712@gmail.com>
2024-03-20 20:04:12 +0300

    changes

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -71,7 +71,20 @@ const CreatePost = () => {
         <form className="w-full flex flex-col space-y-4 md:space-y-8 mt-4">
           <input onChange={(e)=>setTitle(e.target.value)} type="text" placeholder='Enter post title' className="px-4 py-2 outline-none" />
           <input onChange={(e)=>setFile(e.target.files[0])} type="file" className="px-4 " />
-          <div className=""></div>
+          <div className="flex flex-col">
+            <div className="flex items-center space-x-4 md:space-x-8">
+              <input value={cat} onChange={(e)=>setCat(e.target.value)} className='px-4 py-2 outline-none' placeholder='Enter post category' type="text" />
+              <div className="bg-black text-white px-4 py-2 font-semibold cursor-pointer">Add</div>
+            </div>
+            <div className="flex px-4 mt-3">
+              {cat?.map((c,i)=> (
+                <div className="flex justify-center items-center space-x-2 mr-4 bg-gray-200 px-2 py-1 rounded-md">
+                  <p>{c}</p>
+                  <p onClick={()=>deleteCategory(i)} className='text-white bg-black rounded-full cursor-pointer p-1 text-sm'><ImCross/></p>
+                </div>
+              ))}
+            </div>
+          </div>
         </form>
       </div>
     </div>
