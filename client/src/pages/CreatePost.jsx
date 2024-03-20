18798f83b7fa37641196ad315cffd554002e9187
simo blues <wambuisimon712@gmail.com>
2024-03-20 13:48:49 +0300

    changes

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -71,6 +71,7 @@ const CreatePost = () => {
         <form className="w-full flex flex-col space-y-4 md:space-y-8 mt-4">
           <input onChange={(e)=>setTitle(e.target.value)} type="text" placeholder='Enter post title' className="px-4 py-2 outline-none" />
           <input onChange={(e)=>setFile(e.target.files[0])} type="file" className="px-4 " />
+          <div className=""></div>
         </form>
       </div>
     </div>
