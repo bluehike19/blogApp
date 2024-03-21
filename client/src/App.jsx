cdf5aa1ad8e6e8a0a3c3d97991dc63229ce31ec7
simo blues <wambuisimon712@gmail.com>
2024-03-21 06:13:30 +0300

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Login from './pages/Login'
 import Home from './pages/Home'
 import Register from './pages/Register'
 import CreatePost from './pages/CreatePost'
+import PostDetails from './pages/PostDetails'
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
     <Route exact path='/login' element={<Login /> } />
     <Route exact path='/register' element={<Register /> } />
     <Route exact path='/write' element={<CreatePost /> } />
+    <Route exact path='/posts/post/:id' element={<PostDetails /> } />
   </Routes>
  </UserContextProvider>
   )
--- client/src/pages/PostDetails.jsx
+++ client/src/pages/PostDetails.jsx
@@ -1,6 +1,7 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { UserContext } from '../context/UserContext'
+import axios from 'axios'
 
 const PostDetails = () => {
 
@@ -12,6 +13,31 @@ const PostDetails = () => {
   const postId = useParams().id
   const navigate = useNavigate()
   const {user} = useContext(UserContext)
+
+  const fetchPost = async()=> {
+    try {
+      const res = await axios.get(URL+"/api/posts/"+postId)
+      setPost(res.data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const handleDeletePost = async()=> {
+    try {
+      const res = await axios.delete(URL+"/api/posts/"+postId,{withCredentials:true})
+      console.log(res.data)
+      navigate("/")
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(()=>{
+    fetchPost()
+  },[postId])
+
+  
 
   return (
     <div>PostDetails</div>
