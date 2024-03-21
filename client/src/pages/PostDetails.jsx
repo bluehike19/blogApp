56fb508a224177ca2633e632fc43753ee5cbb767
simo blues <wambuisimon712@gmail.com>
2024-03-21 05:56:44 +0300

    changes

--- client/src/pages/PostDetails.jsx
+++ client/src/pages/PostDetails.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { UserContext } from '../context/UserContext'
 
 const PostDetails = () => {
+
+  const [post, setPost] = useState({})
+  const [comments, setComments] = useState([])
+  const [comment, setComment] = useState("")
+  const [loader, setLoader] = useState(false)
+
+  const postId = useParams().id
+  const navigate = useNavigate()
+  const {user} = useContext(UserContext)
+
   return (
     <div>PostDetails</div>
   )
