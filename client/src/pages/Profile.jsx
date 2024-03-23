761de6c42a72652037ae67a54e6ca97ee4ef5965
simo blues <wambuisimon712@gmail.com>
2024-03-23 05:41:10 +0300

    changes

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { UserContext } from '../context/UserContext'
 
 const Profile = () => {
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const {user, setUser} = useContext(UserContext)
+  const [posts, setPosts] = useState([])
+  const [updated, setUpdated] = useState(false) 
+  const param = useParams().id
+  const navigate = useNavigate()
+
+  const fetchProfile = async ()=>{
+    try {
+      const res = await axios.get(URL+"/api/users/"+user._id)
+      setUsername(res.data.username)
+      setEmail(res.data.email)
+      setPassword(res.data.password) 
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  
+
   return (
     <div>Profile</div>
   )
 }
 
-export default Profile
+export default Profile    
