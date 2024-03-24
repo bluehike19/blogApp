fb609df53cff88a127b96c413c4d464ace9fa994
simo blues <wambuisimon712@gmail.com>
2024-03-24 06:31:43 +0300

    changes

--- client/src/pages/EditPost.jsx
+++ client/src/pages/EditPost.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { UserContext } from '../context/UserContext'
+import axios from 'axios'
 
 const EditPost = () => {
 
@@ -13,7 +14,25 @@ const EditPost = () => {
   const [cat, setCat] = useState("")
   const [cats, setCats] = useState([])
 
-  
+  const fetchPosts = async()=>{
+   try {
+     const res = await axios.get(URL+"/api/posts/"+postId)
+   } catch (err) {
+    console.log(err);
+   }
+  }
+
+  const handleUpdate = async (e)=> {
+    e.preventDefault()
+    const post={
+      title,
+      desc,
+      username:user.username,
+      userId:user._id,
+      categories:cats
+    }
+  }
+
   return (
     <div>EditPost</div>
   )
