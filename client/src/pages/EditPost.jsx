93fbd984dc52faae98de0888260be4d65fa0bc8b
simo blues <wambuisimon712@gmail.com>
2024-03-25 06:20:19 +0300

    changes

--- client/src/pages/EditPost.jsx
+++ client/src/pages/EditPost.jsx
@@ -72,7 +72,21 @@ const EditPost = () => {
   }
 
   return (
-    <div>EditPost</div>
+    <div>
+      <Navbar/>
+      <div className="px-6 md:px-[200px] mt-8">
+        <h1 className="font-bold md:text-2xl text-xl">Update a post</h1>
+        <form className="w-full flex flex-col space-y-4 md:space-y-8 mt-4">
+          <input onChange={(e)=>setTitle(e.target.value)} value={title} type="text" placeholder='Enter post title' className='px-4 py-2 outline-none' />
+          <input onChange={(e)=>setFile(e.target.value)} type="file" className='px-4' />
+          <div className="flex flex-col">
+            <div className="flex items-center space-x-4 md:space-x-8">
+              <input type="text" />
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
