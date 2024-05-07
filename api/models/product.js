4f31a5fe5a840a75dff05f0e844b3f2bcb214db3
simo blues <wambuisimon712@gmail.com>
2024-05-07 06:38:01 +0300

    changes

--- api/models/product.js
+++ api/models/product.js
@@ -1,5 +1,13 @@
 const mongoose = require("mongoose")
 
 const ProductSchema= mongoose.Schema({
-    
+    name:{
+        type: String,
+        required:[true, "Please enter product name"]
+    },
+    quantity:{
+        type:Number,
+        required: true,
+        default: 0,
+    },
 })
