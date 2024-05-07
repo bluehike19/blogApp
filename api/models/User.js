d78ecd21ab7b3baca65f927d13412bfff39c760e
simo blues <wambuisimon712@gmail.com>
2024-05-07 22:21:27 +0300

    changes

---
+++ api/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+})
+
+module.exports = Mongoose.model("User", UserSchema)
